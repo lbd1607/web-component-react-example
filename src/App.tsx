aa01bca5b26cd6e73e128a5fb08e6f1ac5bba8f2
lbd1607 <35612977+lbd1607@users.noreply.github.com>
2022-08-27 15:35:34 -0500

    add the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import "./App.css";
+import CustomPlayerComponent from "./components/custom-player";
+
+const WIDTH = 640;
+const HEIGHT = 360;
+
+//Avoids error: ts(2339)
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "custom-player": React.DetailedHTMLProps<
+        React.VideoHTMLAttributes<HTMLVideoElement>,
+        HTMLVideoElement
+      >;
+    }
+  }
+}
 
 function App() {
+  <script src="./custom-player.js"></script>;
+  document.onloadedmetadata = () => {
+    customElements.get("custom-player") ||
+      customElements.define("custom-player", CustomPlayerComponent);
+  };
+
+  const [customPlayerElement, setCustomPlayerElement] =
+    useState<Element | null>(null);
+
+  const [elementShadowRoot, setElementShadowRoot] = useState<
+    HTMLVideoElement | null | undefined
+  >(null);
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const setPlayerRef = useCallback(
+    (node: (Element | HTMLVideoElement) | null) => {
+      if (node !== null) {
+        setCustomPlayerElement(node);
+        setElementShadowRoot(
+          customPlayerElement?.shadowRoot?.querySelector("video")
+        );
+      }
+    },
+    [customPlayerElement, elementShadowRoot] //We have to watch these deps because the shadow root state is not set on first render (hooks are async)
+  );
+
+  console.log("customPlayerElement", customPlayerElement); //TBI: Remove
+  console.log("elementShadowRoot", elementShadowRoot); //TBI: Remove
+
+  const handlePlayPause = () => {
+    setIsPlaying(!isPlaying);
+    !isPlaying ? elementShadowRoot?.play() : elementShadowRoot?.pause();
+  };
+
+  const handleFullscreen = () => {
+    elementShadowRoot?.requestFullscreen();
+  };
+
+  const handleSeektoStart = () => {
+    if (elementShadowRoot) {
+      elementShadowRoot.currentTime = 0;
+    }
+  };
+
+  const handleSeektoHalf = () => {
+    if (elementShadowRoot) {
+      elementShadowRoot.currentTime = elementShadowRoot.duration / 2;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "grid",
+        padding: "4rem",
+        justifyContent: "center",
+      }}
+    >
+      <custom-player width={WIDTH} height={HEIGHT} ref={setPlayerRef} />
+      <div id="control-bar">
+        <button onClick={handlePlayPause}>Play/Pause</button>
+        <button onClick={handleFullscreen}>Fullscreen</button>
+        <button onClick={handleSeektoStart}>Seek to start</button>
+        <button onClick={handleSeektoHalf}>Seek to Halfway point</button>
+      </div>
     </div>
   );
 }
